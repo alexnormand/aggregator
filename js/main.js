f205b39f25b17c41a0c17b2597c4b08eff53268e
Alex Normand <anormnd@gmail.com>
2012-03-13 23:01:01 +0100

    added fastclick.js for native-like tapping on touchscreen devices

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ require.config({
     paths : {
 	text: 'lib/requirejs/text',
 	order: 'lib/requirejs/order',
+	fastclick: 'lib/fastclick',
 	bootstraptransition: 'lib/bootstrap-transition',
 	bootstrapcollapse: 'lib/bootstrap-collapse',
 	underscore: 'lib/underscore',
@@ -10,17 +11,21 @@ require.config({
 });
 
 require(
-    ['jquery',
+    ['jquery',     
      'underscore',
      'backbone', 
      'order!routers/router',
+     'fastclick',
      'bootstraptransition', 
      'bootstrapcollapse'], 
-    function ($, _, Backbone, Router) {
+    function ($, _, Backbone, Router, FastClick) {
 
 	var router = new Router;
 	Backbone.history.start();
 
+	//Add FastClick for native-like tapping on smartphones
+	new FastClick(document.body);
+	
 	if(!window.location.hash)
 	    router.navigate('/', true);	
 		
