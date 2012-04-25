ddba3be3ef03fb51691b89f61513793450e28ae9
Alex Normand <anormnd@gmail.com>
2012-04-25 19:29:03 +0200

    updated response objectt by using app->json help method instead of Response

--- get/index.php
+++ get/index.php
@@ -3,7 +3,7 @@ require_once __DIR__.'/silex.phar';
 require_once __DIR__.'/goutte.phar';
 use Goutte\Client;
 use Symfony\Component\DomCrawler\Crawler;
-use Symfony\Component\HttpFoundation\Response;
+
 
 $app = new Silex\Application();
 
@@ -48,21 +48,18 @@ $app['getQuotes'] = $app->protect(function($site) {
 	       'content' => $content);    
     });
 
-  return isset($site['offset']) ? json_encode(array_slice($result, $site['offset']))
-                                : json_encode($result);
+  return isset($site['offset']) ? array_slice($result, $site['offset'])
+                                : $result;
 });
 
 
 $app->get('/{quotesite}', function ($quotesite) use ($app) {    
 
-    $response = new Response(
-		  $app['getQuotes']($app['quotesite_list'][$quotesite]),			
-		  200,
-		  array('Content-Type' => 'application/json')
-		);   
+    $response = $app->json($app['getQuotes']($app['quotesite_list'][$quotesite]));
     $response->setMaxAge(7*60);
     return $response;
 
 })->assert('quotesite', $app['sites_regex']);  
 
 $app->run();
+
